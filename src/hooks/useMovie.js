17c2d73e108ca8ba61635e12ade3939e2aeac175
Sandra <sandra27popielarczyk@gmail.com>
2025-08-06 09:17:50 -0300

    buscando links utlies de la api para hacerle llamadas de diferentes secciones

--- src/hooks/useMovie.js
+++ src/hooks/useMovie.js
@@ -1,1 +1,17 @@
 const apiKey = import.meta.env.VITE_API_KEY;
+
+// Popular movies
+
+// https://api.themoviedb.org/3/movie/popular
+
+// Ultimos lanzamientos
+
+// https://api.themoviedb.org/3/movie/now_playing
+
+// Buscar peli
+
+// https://api.themoviedb.org/3/search/movie
+
+// Movie details
+
+// https://api.themoviedb.org/3/movie/{movie_id}
