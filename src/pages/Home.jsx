0ac33cb25f5f80dbcb15ce16cb95f75ec6be95ff
Sandra <sandra27popielarczyk@gmail.com>
2025-08-09 20:16:02 -0300

    agregando la pagina home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,41 @@
 
+import { Box, Container } from '@mui/material';
+import { useMovies } from '../hooks/useMovies';
+import HeroCarousel from '../components/HeroCarousel';
+import MovieSection from '../components/MovieSection';
 
-export const Home = () => {
+export default function Home() {
+  const nowPlaying = useMovies({ type: 'now_playing', page: 1 });
+  const popular    = useMovies({ type: 'popular', page: 1 });
+  const topRated   = useMovies({ type: 'top_rated', page: 1 });
+
   return (
-    <div>Home</div>
-  )
+    <Container maxWidth="xl" sx={{ py: 2 }}>
+      <HeroCarousel data={nowPlaying.data} loading={nowPlaying.loading} />
+
+      
+      <Box
+        sx={{
+          display: 'flex',
+          gap: 10,                 
+          alignItems: 'stretch', 
+          mt: 6,
+          mx: 10
+        }}
+      >
+        <MovieSection
+          title="Películas Populares"
+          data={popular.data}
+          loading={popular.loading}
+          sx={{ flex: 1 }}      
+        />
+        <MovieSection
+          title="Películas Mejor Puntadas"
+          data={topRated.data}
+          loading={topRated.loading}
+          sx={{ flex: 1 }}    
+        />
+      </Box>
+    </Container>
+  );
 }
